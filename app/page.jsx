933c4f77f7eab59705dd00441d74158e75ae9fd8
Lê  Hoàng Vũ <68913@thegioididong.com>
2023-05-08 22:49:13 +0700

    auth

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,25 @@
 
+import Feed from "@components/Feed"
 
 const Home = () => {
   return (
     <div>
-        <section className="w-full flex-center flex-col">
+      <section className="w-full flex-center flex-col">
+        <h1 className="head_text text-center">
+          Discover & share &nbsp;
+          <br className="max-md:hidden" />
+          <span className="orange_gradient text-center">
+            AI-Powered Prompts
+          </span>
+        </h1>
+        <p className="desc text-center">
+          Promtopia is anopen-source AU prompting tool for modern world to discover,
+          create and share creative prompts
+        </p>
+        
+        <Feed />
 
-        </section>
+      </section>
     </div>
   )
 }
