bbfd725f7e9b754c254dd8bd1e23afd1df565311
Lê  Hoàng Vũ <68913@thegioididong.com>
2023-05-12 16:53:30 +0700

    ...

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -2,17 +2,17 @@ import { connectToDB } from "@utils/database"
 import Prompt from "@models/prompt"
 
 export const GET = async (req, res) => {
-    // const { search_query } = await req.json()
+    const { search_query } = await req.json()
     let prompts
     try {
         await connectToDB()
 
-        prompts = await Prompt.find({}).populate('creator')
-        // if (!search_query) {
-        //     prompts = await Prompt.find({}).populate('creator')
-        // } else {
-        //     prompts = await Prompt.find({ $text: { $search: search_query } }).populate('creator')
-        // }
+        // prompts = await Prompt.find({}).populate('creator')
+        if (!search_query) {
+            prompts = await Prompt.find({}).populate('creator')
+        } else {
+            prompts = await Prompt.find({ prompt: `/${search_query}/` })
+        }
 
         return new Response(JSON.stringify(prompts), { status: 200 })
     } catch (error) {
